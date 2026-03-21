e3cc6e9f96e9574753955db80b036a58d4ac783c
kivattt <kivattt@gmail.com>
2026-03-21 19:49:57 +0100

    sw-render: refactor tests, add color conversion test

--- sw-render/tests/test.c
+++ sw-render/tests/test.c
@@ -16,59 +16,42 @@ void print_green(const char *s) {
 	printf("\x1b[32m%s\x1b[0m", s);
 }
 
+void rect_print(struct Rect r) {
+	printf("x: %i, y: %i, w: %i, h: %i\n", r.x, r.y, r.w, r.h);
+}
+
 struct TestCaseRectIntersect {
 	struct Rect a;
 	struct Rect b;
 	struct Rect expected;
 };
 
-struct TestCaseRectIntersect helper(int32_t x1, int32_t y1, int32_t w1, int32_t h1, int32_t x2, int32_t y2, int32_t w2, int32_t h2, int32_t x_expected, int32_t y_expected, int32_t w_expected, int32_t h_expected) {
-	struct Rect a;
-	a.x = x1;
-	a.y = y1;
-	a.w = w1;
-	a.h = h1;
-
-	struct Rect b;
-	b.x = x2;
-	b.y = y2;
-	b.w = w2;
-	b.h = h2;
-
-	struct Rect expected;
-	expected.x = x_expected;
-	expected.y = y_expected;
-	expected.w = w_expected;
-	expected.h = h_expected;
-
-	struct TestCaseRectIntersect out;
-	out.a = a;
-	out.b = b;
-	out.expected = expected;
-	return out;
-}
-
-void rect_print(struct Rect r) {
-	printf("x: %i, y: %i, w: %i, h: %i\n", r.x, r.y, r.w, r.h);
-}
-
 int test_rect_intersect() {
 #define NUM_INTERSECT_TESTS 4
 	struct TestCaseRectIntersect tests[NUM_INTERSECT_TESTS] = {
-		helper(0, 0, 100, 100,
-		       50, 50, 100, 100,
-		       50, 50, 50, 50),
-		helper(50, 50, 100, 100,
-		       0, 0, 100, 100,
-		       50, 50, 50, 50),
+		{
+			.a = {0, 0, 100, 100},
+			.b = {50, 50, 100, 100},
+			.expected = {50, 50, 50, 50},
+		},
 
-		helper(0, 0, 100, 100,
-		       30, 80, 40, 100,
-		       30, 80, 40, 20),
+		{
+			.a = {50, 50, 100, 100},
+			.b = {0, 0, 100, 100},
+			.expected = {50, 50, 50, 50},
+		},
 
-		helper(0, 0, 10, 10,
-		       20, 20, 10, 10,
-		       0, 0, 0, 0)
+		{
+			.a = {0, 0, 100, 100},
+			.b = {30, 80, 40, 100},
+			.expected = {30, 80, 40, 20},
+		},
+
+		{
+			.a = {0, 0, 10, 10},
+			.b = {20, 20, 10, 10},
+			.expected = {0, 0, 0, 0},
+		},
 	};
 
 	for (int i = 0; i < NUM_INTERSECT_TESTS; i++) {
@@ -90,12 +73,24 @@ int test_rect_intersect() {
 	return 0;
 }
 
+int test_abgr_to_argb() {
+
+	print_green(" Success\n");
+	return 0;
+}
+
 int main() {
 	int result;
 	int failed = 0;
 
 	print_gray("test_rect_intersect():");
 	result = test_rect_intersect();
+	if (result != 0) {
+		failed = 1;
+	}
+
+	print_gray("test_abgr_to_argb():");
+	result = test_abgr_to_argb();
 	if (result != 0) {
 		failed = 1;
 	}
