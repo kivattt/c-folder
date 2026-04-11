b7e71e5745a36436f35a99a60d40d26ccccc468d
kivattt <kivattt@gmail.com>
2026-04-11 21:35:13 +0200

    taskbar: add missing protocols folder

---
+++ taskbar/protocols/gen_protocol_headers.sh
@@ -0,0 +1,5 @@
+wayland-scanner client-header wlr-layer-shell-unstable-v1.xml wlr-layer-shell-client-protocol.h
+wayland-scanner private-code wlr-layer-shell-unstable-v1.xml wlr-layer-shell-protocol.c
+
+wayland-scanner client-header xdg-shell.xml xdg-shell-client-protocol.h
+wayland-scanner private-code xdg-shell.xml xdg-shell-protocol.c
