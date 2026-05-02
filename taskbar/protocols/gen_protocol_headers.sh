535056fe035c7319a5b03728041330a61817131f
kivattt <kivattt@gmail.com>
2026-05-02 19:09:22 +0200

    taskbar: add xdg-output protocol

--- taskbar/protocols/gen_protocol_headers.sh
+++ taskbar/protocols/gen_protocol_headers.sh
@@ -3,3 +3,6 @@ wayland-scanner private-code wlr-layer-shell-unstable-v1.xml wlr-layer-shell-pro
 
 wayland-scanner client-header xdg-shell.xml xdg-shell-client-protocol.h
 wayland-scanner private-code xdg-shell.xml xdg-shell-protocol.c
+
+wayland-scanner client-header xdg-output-unstable-v1.xml xdg-output-client-protocol.h
+wayland-scanner private-code xdg-output-unstable-v1.xml xdg-output-protocol.c
