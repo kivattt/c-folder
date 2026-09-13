cef76e9ddd3f9f53ad9a7efffbd5fce7f630caa3
kivattt <kivattt@gmail.com>
2026-09-13 23:10:20 +0200

    fontbmp: fix demo

--- fontbmp/demo/demo.c
+++ fontbmp/demo/demo.c
@@ -4,13 +4,13 @@
 #include "../fontbmp.h"
 
 int main() {
-	struct Font font = fontbmp_initialize();
+	struct FontBMPFont font = fontbmp_initialize();
 
 	FT_Error err = fontbmp_generate(&font, "Inter-Regular.ttf", 40);
 	if (err) printf("err: %i\n", err);
 
 	for (int c = 0; c <= 255; c++) {
-		struct GlyphBitmap glyph = font.glyph_list[c];
+		struct FontBMPGlyphBitmap glyph = font.glyph_list[c];
 		printf("Char %c, index %i, width: %i, rows: %i, pitch: %i, data: %p\n", (char)c, c, glyph.width, glyph.rows, glyph.pitch, glyph.bitmap_data);
 	}
 
