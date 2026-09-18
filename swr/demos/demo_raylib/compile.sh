268a4e0079becf8dbc66ef57badb56453fa176f3
kivattt <kivattt@gmail.com>
2026-09-18 21:48:15 +0200

    swr: add raylib demo

---
+++ swr/demos/demo_raylib/compile.sh
@@ -0,0 +1,2 @@
+CC="${CC:-gcc}"
+$CC -Wall -Wextra -Wconversion -O3 -march=native demo.c -o demo $(pkg-config --cflags freetype2) -lfreetype -lm -lraylib -lX11
---
+++ swr/demos/demo_raylib/demo.c
@@ -0,0 +1,75 @@
+#include <assert.h>
+#include <raylib.h>
+#include <time.h>
+
+#include "../../swr.h"
+
+int main() {
+	int image_width = 3840;
+	int image_height = 2560;
+
+	struct swr_output r;
+	swr_initialize(&r);
+
+	size_t image_size = (size_t)(4 * image_width * image_height);
+	uint32_t *image = malloc(image_size);
+	swr_set_output(&r, image, image_width, image_height);
+
+	SetTraceLogLevel(LOG_WARNING);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(1280, 720, "sw-render.c demo");
+
+	while (!WindowShouldClose()) {
+		if (IsKeyDown(KEY_Q)) {
+			break;
+		}
+
+		// Clear the frame buffer
+		struct timespec start, end;
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		swr_draw_fill(&r, swr_rgb(0, 0, 0));
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		uint64_t diff = (uint64_t)((end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec));
+		printf("Nanos: %lu\n", diff);
+
+		// Draw text using the default font to the frame buffer
+		swr_draw_text(&r, "Hello world, from default font!", 22, swr_rgb(255, 255, 255), 75, 57);
+
+		// Draw a rounded rectangle outline around the text
+		struct swr_rect rect = {
+			.x = 43,
+			.y = 40,
+			.w = 400,
+			.h = 60,
+		};
+		swr_draw_rectangle_rounded_outline(&r, rect, swr_rgb(60, 220, 80), 10.0, 1.0, 1.0);
+
+		swr_draw_fps(&r, 22, swr_rgb(0,255,0), 0, 100);
+
+		// Raylib wants an ABGR image, but we output in ARGB.
+		// We convert so it looks correct. (Expensive!)
+		//swr_convert_image_abgr_to_argb(image, image_width * image_height);
+
+		// Do NOT call UnloadImage() on this!
+		Image img = {
+			.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
+			.width = image_width,
+			.height = image_height,
+			.data = image,
+			.mipmaps = 1,
+		};
+
+		Texture2D texture = LoadTextureFromImage(img);
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawTexture(texture, 0, 0, WHITE); // Draw our software-rendered image
+		DrawFPS(5, 5);
+		EndDrawing();
+
+		UnloadTexture(texture);
+	}
+
+	CloseWindow();
+	swr_deinitialize(&r);
+}
